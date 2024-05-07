d59300a3dd49f2598e91f301ce7ec35e29493658
NeuamticEye0 <s.rivera1309@gmail.com>
2024-05-07 13:58:15 -0500

    Create NeumaticEye0.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/NeumaticEye0.py
@@ -0,0 +1,14 @@
+#https://www.python.org/
+
+"""
+Cadenas de
+Texto
+"""
+
+Varaible = ""
+Variable_int = 2
+Varible float= 3.5
+Vabiale_bool=True
+Vabiale_bool=False
+Variable_string= "Mamahuevo"
+print (f"Hola,python")
